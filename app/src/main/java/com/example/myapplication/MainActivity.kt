676b0f17709b57d6abb9349b661351c426a5676b
samoylenkodmitry <samoylenkodmitry@gmail.com>
2023-08-17 11:45:47 +0300

    This can be a workaround, however not guaranteed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,6 +17,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.delay
@@ -38,11 +37,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-	var itemsCount by remember { mutableIntStateOf(100) }
+	var itemsCount by remember { mutableIntStateOf(1000) }
 	val state = rememberLazyListState()
 	LaunchedEffect(itemsCount) {
 		delay(1)
-		itemsCount = 100 + (itemsCount + 1) % 200
+		itemsCount = 1000 + (itemsCount + 1) % 2000
 	}
 	LaunchedEffect(itemsCount) {
 		state.animateScrollToItem(itemsCount)
@@ -50,10 +49,15 @@ fun Greeting() {
 	val texts = arrayOf("Hello", "World", "!")
 	LazyColumn(state = state) {
 		items(itemsCount) {
-			Box { // <-- crucial for the crash
-				Text(
-					text = texts.random()
-				)
+			Box {
+				SubcomposeLayout {
+					val placeable = subcompose("text") {
+						Text(texts.random())
+					}.first().measure(it)
+					layout(placeable.width, placeable.height) {
+						placeable.place(0, 0)
+					}
+				}
 			}
 		}
 	}
